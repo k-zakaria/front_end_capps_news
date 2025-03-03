8596960155b7d6d6d1a6fe78705122ea1c2880cb
k-zakaria <kacimizakaria28@gmail.com>
2025-03-03 14:37:25 +0000

    ajouter les premiers cards de page sport

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SportComponent } from './pages/sport/sport.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-      },
+    },
+    {
+        path: 'sport',
+        component: SportComponent,
+    },
     {
         path: 'auth',
         children: [
---
+++ src/app/pages/sport/sport.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FirstCardsComponent } from "../../components/sport/first-cards/first-cards.component";
+
+@Component({
+  selector: 'app-sport',
+  imports: [FirstCardsComponent],
+  templateUrl: './sport.component.html',
+  styleUrl: './sport.component.css'
+})
+export class SportComponent {
+
+}
